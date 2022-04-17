9ffc17506cc5538fb7db679b709b7ccd6ab62b6a
desarrollo.net.cardenas@hotmail.com <desarrollo.net.cardenas@hotmail.com>
2022-04-16 18:40:02 -0600

    closures
    Closures, understanding outer scope and inner scope.

---
+++ closures-v2.js
@@ -0,0 +1,25 @@
+// Closure is a Javascript feature (it's created by Javascript itself) that allows to 
+// access to variables and functions created in the outer scope. 
+// This feature encloses the variables that resides in the outer reference
+// function in order to has access to them in the inner scope.
+function buildFunctions(){
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) { //<-- At this point, variable 'i'
+        arr.push(
+            ((j) => {
+                return () => {
+                    console.log(j); // <--Here I can access to variable 'i' passed as argument.
+                }
+            })(i)   // <-- pass variable 'i' as argument to IIFE
+        );
+    }
+
+    return arr;
+}
+
+var list = buildFunctions(); // This line returns an array of function expressions
+
+list[0]();  // <-- Here execute the function expression in the position 0
+list[1]();
+list[2]();
---
+++ closures.js
@@ -0,0 +1,37 @@
+
+function printHello () {
+    const lastName = 'Cardenas';
+
+    return function showName(param) {
+        const firstName = 'Hector';
+
+        console.log(`${firstName} ${lastName}`);
+        console.log(`Parameter: ${param}`);
+    }
+}
+
+printHello()('Jose Luis');
+
+let _firstname = 'Jose Perez';
+
+function person(){
+    let _firstname, _lastName;
+    return {
+        setFirstName: function(firstName){
+            _firstname = firstName;
+        },
+        setLastName: function(lastName){
+            _lastName = lastName;
+        },
+        printName: function(){
+            console.log(`My name is: ${_firstname} ${_lastName}`);
+        }
+    };
+}
+
+const pablo = person();
+
+pablo.setFirstName('Hector Pablo');
+pablo.setLastName('Cardenas');
+pablo.printName();
+
